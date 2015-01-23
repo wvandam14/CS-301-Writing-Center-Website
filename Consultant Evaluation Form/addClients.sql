1041c15372afba2834233fbb44c103159988b888
jtalbot14 <jtalbot14@my.whitworth.edu>
2015-01-23 11:28:21 -0800

    Create addClients.sql
    This is an example of how to insert data into the database if you don't want to go through the forms. Or in the case of adding clients and consultants, I don't believe we have a form for that yet.

---
+++ Consultant Evaluation Form/addClients.sql
@@ -0,0 +1,6 @@
+use WritingCenter;
+
+INSERT INTO Clients VALUE (NULL, "John", "Talbot", 4, "Mathematics", NULL);
+INSERT INTO Clients VALUE (NULL, "Trisha", "Talbot", 2, "English", NULL);
+INSERT INTO Clients VALUE (NULL, "Sara", "Annis", 3, "Art", NULL);
+INSERT INTO Clients VALUE (NULL, "John", "Paul", 4, "Chemistry", 1);
