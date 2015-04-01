f7dd18551698415076a64b52e2800c6a8195f41f
jhoole <jenna.hoole@gmail.com>
2015-03-31 20:19:34 -0700

    Created consultant availability files with pseudocode

---
+++ html/consultantAvailability.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+	//session-y stuff
+	//check to make sure user has permission to view this page
+		//error message if they don't
+?>
+<html>
+<head>
+	<!--all your standard head stuff-->
+</head>
+<body>
+	<!--Table with days and times. Dynamically create with javascript? I guess it's also in a form because submit-->
+		<!--when the user clicks on a cell, it changes color and marks status as '1'--handle with javascript-->
+	<!--hidden input with user information pulled from session? Can I do that?-->
+	<!--Submit the form! Yay!-->
+		
+</body>
+</html>
---
+++ php/consultantAvailability.php
@@ -0,0 +1,16 @@
+<?php
+	//session checks and stuff necessary?
+	//will this have any html or just variable handling? dunno.
+		//Gonna need to to continue to navagate site
+	
+	//POST user info
+	//POST all variables --probably have the values in an array
+		//let's do seven arrays. That would make life so much easier.
+		//so that would be a large array with all the values, need to split up
+	//For 0 to 6
+		//take each index in that numbered array and make all into large string
+		//write user ID, day ID, string into database.
+		
+	//Some stuff output because page and whatnot	
+	
+?>	
