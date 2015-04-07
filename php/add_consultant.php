2390a8b585ed0a4c557f46f9cbfe2b8ce70dce18
Joey Corry <jcorry.git@gmail.com>
2015-04-06 20:14:51 -0700

    Fixed consultant functionality

--- php/add_consultant.php
+++ php/add_consultant.php
@@ -5,21 +5,7 @@
 		die('Connect error: '.$db->correct_errno);
 	}
 
-	$stmt = "INSERT INTO accounts (fname, lname, email_address, password, accounttypeid, accountdetails) values (?,?,?,?,2,?)";
-	$fname = $db->real_escape_string($_POST['fname']);
-	$lname = $db->real_escape_string($_POST['lname']);
-	$email = $db->real_escape_string($_POST['email']);
-	$password = $db->real_escape_string($_POST['password']);
-	$accountDetailsID = $db->real_escape_string($_POST['accountDetailsID']);
-	$stmt->bind_param("ssssi", $fname, $lname, $email, $password, $accountDetailsID);
-	$stmt->execute();
-
-	$stmt = "SELECT accountid from accounts where fname = ? and lname = ?";
-	$stmt->bind_param("ss", $fname, $lname);
-	$stmt->execute();
-	$id = $stmt->get_result()->fetch_assoc()['accountid'];
-
-	$stmt2 = "INSERT INTO accountdetails (class_standing, graduation_year, major, secondary_major, minor, bio, missed_appointments) values (?,?,?,?,?,?,?)";
+	$stmt = $db->prepare("INSERT INTO accountdetails (class_standing, graduation_year, major, secondary_major, minor, bio, missed_appointments) values (?,?,?,?,?,?,?)");
 	$classStanding = $db->real_escape_string($_POST['classStanding']);
 	$graduationYear = $db->real_escape_string($_POST['graduationYear']);
 	$major = $db->real_escape_string($_POST['major']);
@@ -28,4 +14,13 @@
 	$bio = $db->real_escape_string($_POST['bio']);
 	$missedAppointments = $db->real_escape_string($_POST['missedAppointments']);
 	$stmt->bindParams("sissssi", $classStanding, $graduationYear, $major, $secondaryMajor, $minor, $bio, $missedAppointments);
+	$stmt->execute();
+
+	$stmt = $db->prepare("INSERT INTO accounts (fname, lname, email_address, password, accounttypeid, accountdetails) values (?,?,?,?,2,?)");
+	$fname = $db->real_escape_string($_POST['fname']);
+	$lname = $db->real_escape_string($_POST['lname']);
+	$email = $db->real_escape_string($_POST['email']);
+	$password = $db->real_escape_string($_POST['password']);
+	$accountDetailsID = $db->real_escape_string($db->insert_id);
+	$stmt->bind_param("ssssi", $fname, $lname, $email, $password, $accountDetailsID);
 	$stmt->execute();
