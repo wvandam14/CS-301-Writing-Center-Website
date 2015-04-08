d53a37621f325e30253287c7819e8fb070148927
Jenna Hoole <jenna.hoole@gmail.com>
2015-04-07 20:23:11 -0700

    Updated CSS in availabilityConfirmed.php and postConsultation.php

--- html/availabilityConfirmed.php
+++ html/availabilityConfirmed.php
@@ -13,16 +13,17 @@
 	<title>Availability Confirmed</title>
 	<meta charset="uft-8">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/indexStyle.css">
 </head>
 <body>
 	<h1>Availability Confirmed</h1>
 	
 	<?php 
-		if(!isset($_SESSION['user_id'])){
+		if(!isset($_SESSION['id'])){
 			echo "You are not logged in. Please <a href='login.html'>log in</a> to continue";
 			exit;
 		}
-		if($_SESSION['permission'] == 3){
+		if($_SESSION['type'] == 3){
 			echo "You do not have permission to view this page";
 			exit;
 		}
