fdbadd408c8a9e99294a6f3d23905504d9568b40
Hannah Gamiel <hgamiel15@my.whitworth.edu>
2015-04-07 22:41:34 -0700

    Changed index.htm to index.php to allow for navbar to be added. Changed the redirect in login.php from index.htm to index.php. Edited navbar to not look like a plaintext list.

--- php/navbar.php
+++ php/navbar.php
@@ -1,4 +1,6 @@
 <!-- session permissions: 0 (client), 1 (consultant), 2 (admin) -->
+<link rel="stylesheet" type="text/css" href="../css/indexStyle.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <nav id="username-display">
   <ul>
     <li><a href="../html/index.htm">Home</a></li>
